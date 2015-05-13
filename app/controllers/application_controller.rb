4d5cb09939bf32d82bbfa7b5ac893c28f9eab771
Ryan Haase <ryandhaase@gmail.com>
2015-05-13 19:25:25 -0400

    Fixed syntax error in application controller as identified in heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::API
 
   rescue_from ActionController::UnpermittedParameters do |parameter_unpermitted_exception|
     render json:
-    { "error": "Unpermitted parameter: #{parameter_unpermitted_exception}" }, status: :bad_request
+    { "error" => "Unpermitted parameter: #{parameter_unpermitted_exception}" }, status: :bad_request
   end
 
   protected
