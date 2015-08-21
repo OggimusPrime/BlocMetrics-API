1088c5812e7f8c80e60abc88734e081d449fa888
Ryan Haase <ryandhaase@gmail.com>
2015-08-21 11:08:16 -0700

    Update app name

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-##BlocMetrics API
+##SimpleMetrics API
 
-BlocMetrics API is a basic API Tracking Service to be used the the BlocMetrics Dashboard - Reporting tool.
+SimpleMetrics API is a basic API Tracking Service to be used the the SimpleMetrics Dashboard - Reporting tool.
 
 The standalone API is the data warehouse for all projects(web apps) and events.
 
-####BlocMetrics Dashboard:
+####SimpleMetrics Dashboard:
 
-<a href="https://github.com/OggimusPrime/BlocMetrics">Repository</a>
+<a href="https://github.com/OggimusPrime/SimpleMetrics">Repository</a>
 
-<a href="https://ryanhaase-blocmetrics.herokuapp.com">BlocMetrics</a>
+<a href="https://ryanhaase-simplemetrics.herokuapp.com">SimpleMetrics</a>
 
 ---
 By default, in development, the API boots up on port 3001 and the dashboard runs on 3000.  If you would like to modify this, you can do so in config/boot.rb
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -10,10 +10,10 @@
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
 
-# Although this is not needed for an api-only application, rails4 
-# requires secret_key_base or secret_token to be defined, otherwise an 
+# Although this is not needed for an api-only application, rails4
+# requires secret_key_base or secret_token to be defined, otherwise an
 # error is raised.
 # Using secret_token for rails3 compatibility. Change to secret_key_base
 # to avoid deprecation warning.
 # Can be safely removed in a rails3 api-only application.
-BlocmetricsApi::Application.config.secret_token = '74b89a69799768b60badae0711e5f10c7e92bb0e30c3591134f4612203051c1cb9add9b78f29b399450fe2920714164d5c5b12797ac57c6997f136461d02ae19'
+SimplemetricsApi::Application.config.secret_token = '74b89a69799768b60badae0711e5f10c7e92bb0e30c3591134f4612203051c1cb9add9b78f29b399450fe2920714164d5c5b12797ac57c6997f136461d02ae19'
